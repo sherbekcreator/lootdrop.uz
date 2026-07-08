a9bfbb69e959678a34cc359861faa5ff4ffdd8e8
sherbekcreator <sherbekshavkatov4939@gmail.com>
2026-07-09 00:02:42 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ def keep_alive():
 TOKEN = '8610358967:AAEa0hxqrnKA8z6xfwS5oVF5yXc8l7VwSj4'
 bot = telebot.TeleBot(TOKEN, threaded=True)
 
-WEB_APP_URL = "https://sherbekcreator.github.io/loottap-bot/"
+WEB_APP_URL = "https://sherbekcreator.github.io/lootdrop.uz/"
 START_IMAGE_URL = "https://raw.githubusercontent.com/sherbekcreator/loottap-bot/main/banner.jpg"
 
 # SIZNING UZCARD / HUMO RAQAMINGIZ (KASSA UCHUN)
